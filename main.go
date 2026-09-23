652ce71624af3006ab39e6b97a7825525bab9a8f
Diti Bhattacharjee Trisha <ditibhattacharjeetrisha@gmail.com>
2026-09-23 17:39:57 +0600

    Add files via upload

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+var (
+	counter int = 1
+	lastt       = time.Now()
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	var possible bool
+	tn := time.Now()
+	if r.Method == http.MethodGet {
+		counter++
+		if tn.Sub(lastt) < 1*time.Second {
+			if counter > 20 {
+				log.Printf("Can not process this for %v", lastt.Add(1*time.Second).Sub(tn))
+			} else {
+				possible = true
+			}
+		} else {
+			possible = true
+			lastt = tn
+			counter = 1
+		}
+	} else {
+		counter++
+		if tn.Sub(lastt) < 1*time.Second {
+			if counter > 5 {
+				log.Printf("Can not process this for %v", lastt.Add(1*time.Second).Sub(tn))
+			} else {
+				possible = true
+			}
+		} else {
+			possible = true
+			lastt = tn
+			counter = 1
+		}
+	}
+	if possible {
+		log.Println("Processed")
+	}
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Println("Server is online on localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
+
+// one request : curl -i -X POST localhost:8080
+// multiple request : for /l %i in (1, 1, 10) do curl -i -X POST localhost:8080
